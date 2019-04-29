593081c2de66c54e37e861bb2713ab9a8a822835
Dawson Richey <dawsonrichey89@gmail.com>
2019-04-29 04:55:30 -0400

    initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { EventsAppComponent } from './events-app.component';
+import { EventsListComponent } from './events/events-list.component'
+import { EventThumbnailComponent } from './events/event-thumbnail.component'
+import { NavBarComponent } from './nav/nav-bar.component'
+import { EventService } from './events/shared/event.service'
+import { ToastrService } from './common/toastr.service'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent
+  ],
+  providers: [EventService, ToastrService],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
